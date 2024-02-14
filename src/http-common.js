2861a7cfaec1239a668f5fac655893fef417945a
joeroseo <134557612+joeroseo@users.noreply.github.com>
2024-02-14 14:33:57 -0500

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://nodejoe5.azurewebsites.net/api",
+  baseURL: "https://nodejoe5.azurewebsites.net/api",
   headers: {
     "Content-type": "application/json"
   }
