b3fcdce6cfcc0f5a5e0a771d7cb48d39e370164a
joeroseo <joerosejax@gmail.com>
2024-02-14 13:09:17 -0500

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   devServer: {
-    port: 8081
+    port: 8081,
+    host: '0.0.0.0'
   }
+  productionSourceMap: false,
+    configureWebpack: {
+        performance: {
+            hints: process.env.NODE_ENV === 'production' ? 'warning' : false
+        }
+    }
 }
