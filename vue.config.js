ba57a93090c59ecd57586e7313301a8eb6982986
joeroseo <joerosejax@gmail.com>
2024-02-14 13:13:33 -0500

    Comitt2

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,5 @@ module.exports = {
   devServer: {
     port: 8081,
     host: '0.0.0.0'
-  }
-  productionSourceMap: false,
-    configureWebpack: {
-        performance: {
-            hints: process.env.NODE_ENV === 'production' ? 'warning' : false
-        }
-    }
+  },
 }
